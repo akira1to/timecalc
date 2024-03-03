e5875c4047587ff21fd319bbd839ac847c7f4261
akira1to <95665186+akira1to@users.noreply.github.com>
2024-03-03 10:27:09 +0100

    Create time.sh

---
+++ time.sh
@@ -0,0 +1,28 @@
+#!/bin/bash
+
+printf "\nEnter number: "
+declare -i minutes0
+declare +i minutes
+minutes0=0;
+read -r minutes && printf -v minutes0 '%i' $minutes
+
+declare -i hours=0
+
+if [[ $(($minutes - 60)) -ge 0 ]]
+then
+	while (( $minutes - 60 >= 0 ))
+	do
+		hours=$hours+1
+		minutes=$(($minutes - 60))	
+	done
+
+	printf "\n$hours"
+	printf "h "
+	printf "$minutes"
+	printf "m\n"
+else
+	printf "\n$hours"
+	printf "h "
+	printf "$minutes"
+	printf "m\n"
+fi
